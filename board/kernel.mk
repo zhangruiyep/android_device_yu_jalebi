e5e3c28a156e68589cd50205d84fab2bf70ede5c
Prateek Chaubey <chaubeyprateek@gmail.com>
2017-09-18 17:14:47 +0000

    jalebi: Add Toolchain Path

--- board/kernel.mk
+++ board/kernel.mk
@@ -3,3 +3,5 @@ BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 TARGET_KERNEL_CONFIG := lineageos_jalebi_defconfig
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
